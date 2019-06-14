1c1ca03a2debf36e56df7f88545798c92879b36c
gmiklay <gabor.miklay.74@gmail.com>
2019-06-14 14:11:25 +0200

    androidX

--- app/src/main/java/info/androidhive/rxjavasearch/adapter/ContactsAdapter.java
+++ app/src/main/java/info/androidhive/rxjavasearch/adapter/ContactsAdapter.java
@@ -5,7 +5,7 @@ package info.androidhive.rxjavasearch.adapter;
  */
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
